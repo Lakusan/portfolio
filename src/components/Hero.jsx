371fd83ba87585fa9eae40a8c81ec9837a2e23fe
Lakusan <47719054+Lakusan@users.noreply.github.com>
2024-11-29 11:21:59 +0100

    added typewriter effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,8 @@
 import { HERO_CONTENT } from '../constants';
 import profilePic from '../assets/kevinRushProfile.png';
 import { motion, transform } from 'framer-motion';
-import { SiOppo } from 'react-icons/si';
+import Typewriter from "typewriter-effect";
+
 
 const container = (delay) => ({
     hidden: { x: -100, opacity: 0 },
@@ -21,9 +22,12 @@ const Hero = () => {
             <div className='flex flex-wrap'>
                 <div className='w-full lg:w-1/2'>
                     <div className='flex flex-col items-center lg:items-start'>
+                        <h1 className='text-white'>
+                            Hi, I'm
+                        </h1>
                         <motion.h1
                             variants={container(0)}
-                            initial='hidden' 
+                            initial='hidden'
                             animate='visible'
                             className='pb-16 text-6xl front-thin tracking-tight lg:mt-16 lg:text-8xl'>
                             Andreas Lakus
@@ -34,8 +38,37 @@ const Hero = () => {
                             initial='hidden'
                             animate='visible'
                         >
-                            Solution Developer
                         </motion.span>
+                            <p>
+                                I do
+                            </p>
+                                <p className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-light text-transparent'>
+                                <Typewriter
+                                    options={{
+                                        strings: ["Git/Dev(Sec)Ops", "Cloud Engineering", "Ai & Computer Vision", "Photogrametry", "IT-Infrastructure", "Software & Solution Development", "Consulting", "Leading Teams", "Service Management"],
+                                        autoStart: true,
+                                        loop: true,
+                                        loopCount: Infinity,
+                                        deleteSpeed: "natural",
+                                        pauseFor: 1000,
+                                    }}
+                                />
+                                </p>
+                    </div>
+
+                    <div>
+
+
+
+
+                        {/* <motion.span
+                            className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-light text-transparent'
+                            variants={container(0.5)}
+                            initial='hidden'
+                            animate='visible'
+                        >
+                            Solution Developer
+                        </motion.span> */}
                         <motion.p
                             variants={container(1)}
                             initial='hidden'
