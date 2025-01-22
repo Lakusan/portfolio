300975d894604b2635af69582ae59752556b307f
Lakusan <47719054+Lakusan@users.noreply.github.com>
2025-01-22 07:51:37 +0100

    data change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -54,13 +54,6 @@ const Contact = () => {
                 </motion.div>
                 <motion.p
                     whileInView={{ opacity: 1, x: 0 }}
-                    initial={{ opacity: 0, x: -100 }}
-                    transition={{ duration: 1 }}
-                    className='my-4'>
-                    {CONTACT.address}
-                </motion.p>
-                <motion.p
-                    whileInView={{ opacity: 1, x: 0 }}
                     initial={{ opacity: 0, x: 100 }}
                     transition={{ duration: 1 }}
                     className='my-4'>{CONTACT.phoneNo}</motion.p>
